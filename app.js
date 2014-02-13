e6e59344c6f5065aacd57d5c8ee505c77cd01ed6
dhrusmart <dhrusmart@gmail.com>
2014-02-13 23:07:20 +0530

    hey please use these file rahul

---
+++ app.js
@@ -0,0 +1,44 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express');
+var http = require('http');
+
+var app = express();
+
+// all environments
+app.configure(function(){
+
+	app.set('port', process.env.PORT || 3000);
+	app.set('view engine', 'jade');
+	app.set('views', __dirname + '/views');
+	//app.use(express.static(path.join(__dirname, 'public')));
+	
+});
+
+
+app.get('/', function(req, res){
+	res.send("Welcome Successful");
+});
+
+app.get('/oneone', function(req, res){
+	res.render("oneone.jade");
+});
+
+app.get('/twoone', function(req, res){
+	res.render("twoone.jade");
+});
+
+app.get('/twotwo', function(req, res){
+	res.render("twotwo.jade");
+});
+
+app.get('/twothree', function(req, res){
+	res.render("twothree.jade");
+});
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
