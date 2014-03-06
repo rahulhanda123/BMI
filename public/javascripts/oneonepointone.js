40a3c96f667b380a806bd8f7f10ba4bb63ec8c36
dhrusmart <dhrusmart@gmail.com>
2014-03-06 21:10:06 +0530

    droopy

---
+++ public/javascripts/oneonepointone.js
@@ -0,0 +1,18 @@
+function saveContents()
+{
+    var f = document.getElementById("facts").value;
+    alert(f);
+}
+
+function prevFunction()
+{
+    saveContents();
+    //alert("Previous Button Clicked");
+    return true;
+}
+function nextFunction()
+{
+    saveContents();
+    //alert("Next Button Clicked");
+    return true;
+}
---
+++ public/javascripts/oneonepointtwo.js
@@ -0,0 +1,17 @@
+function saveContents()
+{
+    var f = document.getElementById("Insights").value;
+    alert(f);
+}
+function prevFunction()
+{
+    saveContents();
+    //alert("Previous Button Clicked");
+    return true;
+}
+function nextFunction()
+{
+    saveContents();
+    //alert("Next Button Clicked");
+    return true;
+}
